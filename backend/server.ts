0cb84ac37eda420619d585049924a38eb16aadcd
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-09-30 19:04:42 -0400

    fixing .gitignore issues

--- backend/server.ts
+++ backend/server.ts
@@ -2,3 +2,7 @@ import * as express from "express";
 console.log("hello gang");
 
 const app = express();
+app.use(express.json());
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+})
