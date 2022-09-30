537dc7d1fc101256ea67a5e9ad03d148cd89e248
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-09-30 19:27:55 -0400

    created env file

--- backend/server.ts
+++
@@ -1,8 +0,0 @@
-import * as express from "express";
-console.log("hello gang");
-
-const app = express();
-app.use(express.json());
-app.listen(3000, () => {
-    console.log("listening on port 3000");
-})
---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { PORT } from "./utils/config";
+
+const app = express();
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
