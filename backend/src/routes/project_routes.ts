c298627a5125dd46dcd913985f5e299fb9949bf6
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-10-07 16:39:16 -0400

    pre dev branch commit

--- backend/src/routes/project_routes.ts
+++ backend/src/routes/project_routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { createProject, deleteProject, getProject, getProjects, updateProject } from "../controllers/project_controller";
 
-const project_router:express.Router = express.Router();
+const project_router: express.Router = express.Router();
 //get all projects
 project_router.route("/").get(getProjects).post(createProject);
 project_router.route("/:id").get(getProject).put(updateProject).delete(deleteProject);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,27 @@
 import express from "express";
 import project_router from "./routes/project_routes";
-import { MONGO_URI, PORT } from "./utils/config";
-import mongoose from "mongoose";
+import { PORT } from "./utils/config";
+import { DataSource } from "typeorm"
 
-const app:express.Application = express();
+const app: express.Application = express();
 app.use(express.json());
 
 app.use("/api/projects", project_router);
 
-try {
-    mongoose.connect(`${MONGO_URI}`).then()
-} catch(error){
-    console.log(error);
-}
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
+const AppDataSource: DataSource = new DataSource ({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "postgres"
 });
+
+AppDataSource.initialize()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`listening on port ${PORT}`);
+        });
+    })
+    .catch((error) => console.log(error));
+
+//mongosh 'mongodb+srv://cluster0.example.com/testdb!!!!!!!!!?authSource=$external&authMechanism=MONGODB-AWS'
