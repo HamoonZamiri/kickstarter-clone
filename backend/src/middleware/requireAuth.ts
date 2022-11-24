dc0d5ecdb913739ed03be016d39cbfaa286b14b8
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-11-24 00:30:53 -0500

    wrote jwt auth middleware function

---
+++ backend/src/middleware/requireAuth.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request } from "express";
+import { findOneById } from "src/services/userService";
+import { GenericResponse } from "src/utils/interfaces";
+import { verifyJwt } from "src/utils/jwt";
+
+export const requireAuth = async(req: Request, res: GenericResponse, next: NextFunction) => {
+    try {
+        const { authorization } = req.headers;
+        if(!authorization){
+            res.status(401).json({ message: "You are not authorized to make this request" });
+            return;
+        }
+        const token = authorization.split(" ")[1];
+        const decoded = await verifyJwt<{sub: string}>(token, "accessTokenPublicKey");
+        if(!decoded){
+            res.status(401).json({ message: "You are not authorized to make this request" });
+            return;
+        }
+        const user = await findOneById(decoded.sub);
+        if(!user){
+            res.status(401).json({ message: "You are not authorized to make this request" });
+            return;
+        }
+        next();
+    }
+    catch(error){
+        console.log(error);
+        res.status(401).json({ message: "Error while trying to authorize your token" });
+        next(error);
+    }
+}
