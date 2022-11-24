6b1e9c2e7780e4fcbb68e7ea5a540502c4df35d7
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-11-24 00:31:44 -0500

    created interface that extends express Response

--- backend/src/utils/interfaces.ts
+++ backend/src/utils/interfaces.ts
@@ -1,3 +1,17 @@
+import { Response } from "express";
+import { Project } from "src/entities/Project";
+import { User } from "src/entities/User";
+
 export interface ErrorResponse {
     error: string
+}
+export interface GenericResponse extends Response {
+    message: string,
+    data?: User | Project | Project[] | User[] | string,
+    accessToken?: string,
+}
+export interface UserInput {
+    username: string,
+    email: string,
+    password: string,
 }
