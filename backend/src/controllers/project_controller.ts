32bf4d7683229949c490f95030e7f56b08b53ad0
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-11-23 19:01:42 -0500

    added type safety to requests and responses

--- backend/src/controllers/project_controller.ts
+++ backend/src/controllers/project_controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express"
+import { ErrorResponse } from "src/utils/interfaces";
 import { Project } from "../entities/Project";
 import { AppDataSource } from "../server";
 
 
-export const getProjects = async (req: Request, res: Response) => {
+
+export const getProjects = async (req: Request, res: Response<Project[] | ErrorResponse>) => {
     if(req)
         console.log("happy");
     const projects = await AppDataSource.manager.find(Project);
@@ -11,33 +13,40 @@ export const getProjects = async (req: Request, res: Response) => {
         res.status(404).json({ error: "No projects found" });
     }
     else {
-        res.status(200).json({"projects": projects});
+        res.status(200).json(projects);
     }
 };
 
-export const createProject = async (req: Request, res: Response) => {
+export const createProject = async (req: Request, res: Response<Project | ErrorResponse>) => {
     if(!req.body.title){
         res.status(400).json({error: "title is required"});
     }
-    const project = new Project();
-    project.title = req.body.title;
-    const projectSaved = await AppDataSource.manager.save(Project, project);
-    res.status(201).json({"project created": projectSaved});
+
+    const newProject = AppDataSource.manager.create(Project, {title: req.body.title, imgUrl: req.body.imgUrl});
+
+    const projectSaved = await AppDataSource.manager.save(Project, newProject);
+    res.status(201).json(projectSaved);
 };
 
-export const getProject = async (req: Request, res: Response) => {
-    const _id  = parseInt(req.params.id);
-    const project = await AppDataSource.manager.findOneBy(Project, {id: _id});
+export const getProject = async (req: Request, res: Response<Project | ErrorResponse>) => {
+    const id  = parseInt(req.params.id);
+    const project = await AppDataSource.manager.findOneBy(Project, {id: id});
     if (project)
-        res.status(200).json({"project": project});
+        res.status(200).json(project);
     else{
         res.status(404).json({error: "project not found"});
     }
 };
 
-export const updateProject = async (req: Request, res: Response) => {
-    console.log(req);
-    res.status(200).json({"message": `update project with id ${req.params.id}`});
+export const updateProject = async (req: Request, res: Response<Project | ErrorResponse>) => {
+    const _id  = parseInt(req.params.id);
+    const project = await AppDataSource.manager.findOneBy(Project, {id: _id});
+    if (project){
+        res.status(200).json(project);
+    }
+    else{
+        res.status(400).json({error: "project could not be updated"});
+    }
 };
 
 export const deleteProject = async (req: Request, res: Response) => {
