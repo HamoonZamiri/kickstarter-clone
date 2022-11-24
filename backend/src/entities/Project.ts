20a9e8f0270d88cd309814e0d4a02019428ec79a
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-11-23 21:32:18 -0500

    added column for description and toJSON func

--- backend/src/entities/Project.ts
+++ backend/src/entities/Project.ts
@@ -11,9 +11,16 @@ export class Project extends BaseEntity{
     @Column({ nullable: true })
     imgUrl: string;
 
+    @Column({ nullable: false, default: "" })
+    description: string;
+
     @CreateDateColumn()
     createdAt: Date;
 
     @UpdateDateColumn()
     updatedAt: Date;
+
+    toJSON() {
+        return {...this}
+    }
 }
