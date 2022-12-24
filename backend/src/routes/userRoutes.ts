2b4ffab3c93af82598c00431bbdcc9db88a1e5f1
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-12-24 13:15:12 -0500

    created get all users endpoint

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express"
 import { getAllUsers, loginUserHandler, refreshAccessTokenHandler, registerUserHandler } from "../controllers/userController";
+import { requireAuth } from "../middleware/requireAuth";
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUserHandler);
 userRouter.post("/login", loginUserHandler);
 userRouter.get("/refresh", refreshAccessTokenHandler);
-userRouter.get("/", getAllUsers);
+userRouter.get("/", requireAuth, getAllUsers);
 export default userRouter;
